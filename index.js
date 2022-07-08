d72dddea22e56eed68d41bc340e1c5474620982c
Jen Weber <j@jenweber.me>
2022-07-07 21:29:04 -0400

    Button hover and click effects

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const onClick = () => {
+const onClick = function() {
   const val = document.querySelector("#url-input").value
 	const tidied = val.split('?')[0];
 	navigator.clipboard.writeText(tidied)
+	this.innerHTML = 'Copied!'
+	setTimeout(() => this.innerHTML = 'Click to copy the nice URL', 2000)
 }
 
 document.querySelector("#copy-button").addEventListener('click', onClick)
